768b9a7559744fa93d87e271712aa660570353d7
JoanAndresRG <joanandres318@gmail.com>
2021-11-18 21:59:55 -0500

    Se transfire credenciales a entorno local

--- BackEnd/gestionempresa/src/services/mensajeria.service.ts
+++ BackEnd/gestionempresa/src/services/mensajeria.service.ts
@@ -1,7 +1,7 @@
-import { injectable, /* inject, */ BindingScope } from '@loopback/core';
-import {Empleado} from '../models/empleado.model'
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import {Empleado} from '../models/empleado.model';
 
-@injectable({ scope: BindingScope.TRANSIENT })
+@injectable({scope: BindingScope.TRANSIENT})
 export class MensajeriaService {
   constructor() { }
 
@@ -10,20 +10,20 @@ export class MensajeriaService {
    */
 
   EnviarSMS(empleado: Empleado): void {
-    console.log("coneccion");
-    const accountSid = '';
-    const authToken = '';
+    console.log("conexión con Twilio exitosa.");
+    const accountSid = process.env.TWILIO_ACCOUNT_SID;
+    const authToken = process.env.TWILIO_AUTH_TOKEN;
 
     const twilio = require('twilio');
     const client = new twilio(accountSid, authToken);
 
     client.messages
       .create({
-        body: `Mensaje enviado a ${empleado.Nombres} correctamente`,
-        from: '',
-        to: ''
+        body: `${empleado.Nombres} ${empleado.Apellidos}, has sido creado correctamente`,
+        from: '+18039024115',
+        to: '+57' + `${empleado.Telefono}`
       })
-      .then((message:any) => console.log(message.sid));
+      .then((message: any) => console.log(message.sid));
 
   }
 }
